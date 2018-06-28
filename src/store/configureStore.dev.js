8f4930991965748a7e70e48046b628bbf1bdab80
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-06-28 18:37:36 +0200

    feat: integrate with redux-devtools-extension & react-native-debugger

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,19 +1,19 @@
 /* @flow */
 
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
-const logger = createLogger();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const sagaMiddleware = createSagaMiddleware();
 
 export default function configureStore(initialState: ?any) {
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(sagaMiddleware, logger)
+    composeEnhancers(applyMiddleware(sagaMiddleware))
   );
 
   if (module.hot) {
