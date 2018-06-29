7e7dd264b98dd8ffc04acbd78b2e4bbbd38a9193
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-06-29 13:36:30 +0200

    chore: use the native modules

--- android/app/src/main/java/com/croma/MainApplication.java
+++ android/app/src/main/java/com/croma/MainApplication.java
@@ -24,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ImageChooserPackage(),
+          new ColorExtractorPackage()
       );
     }
   };
