8e81df784843300bc00a7519ff00f2ef67d63229
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-06-29 13:56:50 +0200

    feat: make statusbar translucent

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 /* @flow strict */
 
+import { Platform, StatusBar } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import PaletteListContainer from '../containers/PaletteListContainer';
 import ColorListContainer from '../containers/ColorListContainer';
@@ -13,5 +14,14 @@ export default createStackNavigator(
   },
   {
     initialRouteName: 'PaletteList',
+    navigationOptions: {
+      headerStyle:
+        Platform.OS === 'android'
+          ? {
+              paddingTop: StatusBar.currentHeight,
+              height: StatusBar.currentHeight + 56,
+            }
+          : null,
+    },
   }
 );
